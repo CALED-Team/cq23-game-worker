586570d5f3bbdcc13af510d573d8a76fe844c835
Ali Toosi <alirezakhoshghalb@ymail.com>
2023-05-31 20:38:57 +1000

    Fix install script

--- bin/remote_prepare.sh
+++ bin/remote_prepare.sh
@@ -2,10 +2,9 @@ rm -rf ~/codequest || echo "codequest directory doesn't exist, creating it..."
 mkdir -p ~/codequest/repo
 cd ~/codequest || exit
 
-sudo apt update
-sudo apt install -y make python3-pip
-
 curl -fsSL https://get.docker.com -o get-docker.sh
 docker -v || sudo sh get-docker.sh
+
+sudo apt install -y make python3-pip
 
 tmux kill-server || echo "Tmux already not running"
